0857cb055753b680bdc2fac590b68dd425090aa0
ZanaSiaka <zanacoul210@gmail.com>
2024-05-20 05:23:25 +0200

    okay

--- src/Composants/exo2.js
+++ src/Composants/exo2.js
@@ -15,9 +15,8 @@ function Exo2() {
         <input type='text' placeholder='Entrez votre nom ici'
         ref={inputRef}/>
         <input type='mail' placeholder='Entrez votre mail ici' />
-        <input type='tel' placeholder='Entrez votre num"ro ici' />
+        <input type='tel' placeholder='Entrez votre numéro ici' />
         <input type='password' placeholder='Entrez votre mot de passe ici' />
-
         <button type='submit'> Cliquez ici </button>
     </div>
   )
