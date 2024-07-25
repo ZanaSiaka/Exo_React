3c838d8c916e9f94dd823bce599180eeeb4730c7
ZanaSiaka <zanacoul210@gmail.com>
2024-07-25 02:10:23 +0200

    Prochaine Vidéo : Vidéo 131

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 // import logo from './logo.svg';
 import './App.css';
-import Exo1 from './Composants/exo1'
-import Exo2 from './Composants/exo2';
+
 
 
 function App() {
   return (
     <div className="App">
-      <Exo1 />
-      {/* <Exo2 /> */}
+      <h1>Appli Marvel Quiz</h1>
     </div>
   );
 }
